d1764a09b98916f41065197137ae091bcfcc77a2
sadepck <sadepck@gmail.com>
2025-11-28 16:50:34 -0300

    parte2

--- client/dev-dist/sw.js
+++ client/dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-ca84f546'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.q955rn3us64"
+    "revision": "0.9ja2hrssm6g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
